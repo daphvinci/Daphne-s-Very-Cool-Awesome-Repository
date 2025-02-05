8a5f71658808bc4efa0997ad83af4744d046bf34
Daphne Rivera <daphnerivera585@gmail.com>
2025-02-04 21:23:50 -0600

    Update generateMarkdown.js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,12 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (!license) {
+  return "";
+}
+
+  
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -8,11 +14,30 @@ function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  let path = ""; 
+    if (license === "MIT") {
+      path = './utils/mit.md';
+    }
+    if (license === "GNU") {
+      path = './utils/gnu.md';
+    }
+    if (license === "Apache") {
+      path = '.utils/apache.md'
+    }
+    const text = fs.readFileSync(path, 'utf-8');
+    return text;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+  const licenseBadge = renderLicenseBadge (data.license);
+  const licenseText = renderLicenseSection(data.license);
+  fs.writeFileSync("LICENSE", licenseText);
+
+
   return `# ${data.title}
+
 
 `;
 }
